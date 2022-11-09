72cb51da85c14edbeb6e3bdbbe0baef2b67c86e9
Milenaverag <117789141+Milenaverag@users.noreply.github.com>
2022-11-09 17:40:05 -0500

    Add files via upload

---
+++ tc2_VeraMilena/tc2_VeraMilena.sh
@@ -0,0 +1,10 @@
+
+echo -e "\n"
+echo "7 primeras filas"
+head -n 7 AgGCN1.0_properties.csv
+
+echo -e "\n"
+echo "5 ultimas filas"
+head -n 5 AgGCN1.0_properties.csv
+
+
---
+++ tc2_VeraMilena/tc2_VeraMilena2.sh
@@ -0,0 +1,3 @@
+
+cut -d ";" -f1 EigenBetw.tsv | grep "AGAP[0-9]* [5]"$
+
